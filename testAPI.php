c4718561cea96ae48a17b1cd74b2e56bc4cba128
mayoi5011 <46701036+mayoi5011@users.noreply.github.com>
2019-03-11 17:07:47 +0700

    Update testAPI.php

--- testAPI.php
+++ testAPI.php
@@ -1,16 +1,5 @@
+
 <?php
-$ch = curl_init();
-// Disable SSL verification
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// Will return the response, if false it print the response
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// Set the url
-curl_setopt($ch, CURLOPT_URL,"http://127.0.0.1:1880/erdibot");
-// Execute
-$result=curl_exec($ch);
-// Closing
-curl_close($ch);
-// แปลงข้อมูลที่รับมาในรูป json มาเป็น array จะได้ใช้ง่าย ๆ
-$DATA= json_decode($result, true);
-// //dump ข้อมูลออกมาดู
-print_r($DATA);
+$homepage = file_get_contents('127.0.0.1:1880/erdibot');
+echo $homepage;
+?>
