5729e0f03e38432c474082716608754e0c0102e6
mayoi5011 <46701036+mayoi5011@users.noreply.github.com>
2019-03-11 17:32:44 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -3,6 +3,8 @@
 
 
 require "vendor/autoload.php";
+$data = file_get_contents('http://apecpv.cmru.ac.th:1880/erdibot');
+$character = json_decode($data);
 
 $access_token = 'oKfXSEXkmDRNdxcUrkAJrvoJ49gdmeCM+MoMbpLXOKXicGis8P7YsIWT0f5BBYP9h3xjTVxRkUdyQGKr3rg6X5nRmSnIMEe7w9+oZ4fKO8d6rEZeiGruIfNwV5eFmsMoPHbb9fdbw92/nWN/jsPpPgdB04t89/1O/w1cDnyilFU=';
 
@@ -13,10 +15,7 @@ $pushID = 'Ub5bd2d0b18e3e8f76cd94e897f05c654';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$data = file_get_contents('http://apecpv.cmru.ac.th:1880/erdibot');
-$character = json_decode($data);
-
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($character->data);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($character);
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
