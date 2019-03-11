341378b5c327a3f3dd97b42db59e98b6fcb39208
mayoi5011 <46701036+mayoi5011@users.noreply.github.com>
2019-03-11 17:28:23 +0700

    Update testAPI.php

--- testAPI.php
+++ testAPI.php
@@ -1,5 +1,6 @@
 
 <?php
-$homepage = file_get_contents('http://apecpv.cmru.ac.th:1880/erdibot');
-var_dump ($homepage);
+$data = file_get_contents('http://apecpv.cmru.ac.th:1880/erdibot');
+$character = json_decode($data);
+var_dump ($character);
 ?>
