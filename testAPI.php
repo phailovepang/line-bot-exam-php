98811a09537dd0eea9c9bbd6667c1bc0d46ff9ed
mayoi5011 <46701036+mayoi5011@users.noreply.github.com>
2019-03-11 16:59:46 +0700

    Create testAPI.php

---
+++ testAPI.php
@@ -0,0 +1,18 @@
+// Create a curl handle
+// ตัวเลขไอดี เราใช้ตัวแปรมาแทน ได้น่ะครับ 
+//  Initiate curl
+$ch = curl_init();
+// Disable SSL verification
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+// Will return the response, if false it print the response
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+curl_setopt($ch, CURLOPT_URL,"http://127.0.0.1:1880/erdibot");
+// Execute
+$result=curl_exec($ch);
+// Closing
+curl_close($ch);
+// แปลงข้อมูลที่รับมาในรูป json มาเป็น array จะได้ใช้ง่าย ๆ
+$DATA= json_decode($result, true);
+// //dump ข้อมูลออกมาดู
+print_r($DATA);
